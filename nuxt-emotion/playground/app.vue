50ebe192260e08aadd24d14acf675b2bc5397fc8
codebender828 <jbakebwa@163.com>
2023-01-20 21:52:32 +0800

    feat: hydrate all css styles

---
+++ nuxt-emotion/playground/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="styles.root">Nuxt module playground!</div>
+</template>
+
+<script setup>
+import { css } from "@emotion/css";
+
+const styles = reactive({
+  root: css({
+    paddingInline: "16px",
+    paddingBlock: "12px",
+    fontSize: "1rem",
+    fontFamily: "system ui",
+    color: "white",
+    borderRadius: "20px",
+    backgroundColor: "indigo",
+  }),
+});
+</script>
---
+++ nuxt-emotion/src/module.ts
@@ -0,0 +1,32 @@
+import { fileURLToPath } from "url";
+import { defineNuxtModule, addPlugin, createResolver } from "@nuxt/kit";
+
+export interface ModuleOptions {
+  addPlugin: boolean;
+}
+
+declare global {
+  interface Window {
+    /** Serialized SSR IDs for emotion */
+    $emotionSSRIds: string[];
+  }
+}
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: "@nuxtjs/emotion",
+    configKey: "emotion",
+    compatibilty: ">=3.0.0",
+  },
+  defaults: {
+    addPlugin: true,
+  },
+  setup(options, nuxt) {
+    if (options.addPlugin) {
+      const { resolve } = createResolver(import.meta.url);
+      const runtimeDir = fileURLToPath(new URL("./runtime", import.meta.url));
+      nuxt.options.build.transpile.push(runtimeDir);
+      addPlugin(resolve(runtimeDir, "plugin"));
+    }
+  },
+});
