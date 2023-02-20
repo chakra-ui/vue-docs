98cd426cf1877c3edd041fabc45971f006631cca
carwack <sybren.willemot@gmail.com>
2023-02-20 18:02:52 +0100

    feat(components): refactor framework list and card

---
+++ components/content/FrameworkList/FrameworkCard.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { CBox, CFlex, CText } from "@chakra-ui/vue-next"
+
+defineProps<{
+  content: string
+  to: string
+  ariaLabel?: string
+}>()
+
+const NuxtLink = resolveComponent('nuxt-link')
+</script>
+
+<template>
+  <CFlex
+    :as="NuxtLink"
+    prefetch
+    :to="to"
+    :aria-label="ariaLabel"
+    :h="['100%', '200px']"
+    :flex-dir="['column', 'row']"
+    border="1px solid"
+    border-color="gray.100"
+    border-radius="lg"
+    overflow="hidden"
+    transition="box-shadow 0.1s ease-out 0s"
+    :_hover="{ shadow: 'md', textDecoration: 'none' }"
+  >
+    <CFlex :w="['100%', '200px']" :h="['200px', '100%']" justify-content="center" align-items="center" overflow="hidden" bg="gray.100" p="5" flex-shrink="0">
+      <slot name="img">
+        <img src="~/assets/vue-logo.png" alt="Vue logo" loading="lazy" />
+      </slot>
+    </CFlex>
+    <CFlex p="5" flex-dir="column">
+      <CHeading>
+        <slot name="title" />
+      </CHeading>
+      <CText>
+        <slot name="description" />
+      </CText>
+    </CFlex>
+  </CFlex>
+</template>
---
+++ components/content/FrameworkList/FrameworkList.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { CFlex } from "@chakra-ui/vue-next"
+import FrameworkCard from "./FrameworkCard.vue";
+</script>
+
+<template>
+<CFlex
+  as="section"
+  flex-dir="column"
+  gap="6"
+  font-family="body"
+  mt="6"
+> 
+  <FrameworkCard content="Vue 3" to="/getting-started/installation" aria-label="Chakra UI Vue getting started with Vue 3">
+    <template #img>
+      <img src="~/assets/vue-logo.png" alt="Vue logo" loading="lazy" />
+    </template>
+    <template #title>
+      Vue 3
+    </template>
+    <template #description>
+      The Progressive JavaScript Framework.
+      An approachable, performant and versatile framework for building web user interfaces.
+    </template>
+  </FrameworkCard>
+  <FrameworkCard content="Nuxt 3" to="/frameworks/nuxt-3" aria-label="Chakra UI Vue getting started with Nuxt 3">
+    <template #img>
+      <img src="~/assets/nuxt-logo.png" alt="Vue logo" loading="lazy" />
+    </template>
+    <template #title>
+      Nuxt 3
+    </template>
+    <template #description>
+      The Intuitive Web Framework.
+      Build your next Vue.js application with confidence using Nuxt.
+    </template>
+  </FrameworkCard>
+</CFlex>
+</template>
