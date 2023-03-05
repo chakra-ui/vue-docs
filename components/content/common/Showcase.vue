54d9662a584678fa39ca3d4bc9d20f2bb302a6c5
Shyrro <zakaria.sahmane@decathlon.com>
2023-03-05 14:57:31 +0100

    feat(components): add components section and accordion docs
    Signed-off-by: Shyrro <zsahmane@gmail.com>

---
+++ components/content/common/Showcase.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { CCenter } from "@chakra-ui/vue-next"
+</script>
+
+<template>
+  <c-center py="3" border="1px solid" border-color="gray.200" border-radius="5" px="4" my="5">
+    <slot />
+  </c-center>
+</template>
