139cd0ec6c404ae547cd2b281b081efb3c060310
Daniel Olabemiwo <olabemiwodaniel@gmail.com>
2023-05-30 19:45:23 +0100

    chore: add github repository link

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,35 +14,21 @@ const color = useColorModeValue('gray.700', 'white');
     <TopNavigation />
     <chakra.div max-w="8xl" mx="auto" d="flex">
       <!-- Sidebar Navigation -->
-      <chakra.div
-        :display="{ base: 'none', lg: 'block' }"
-        position="fixed"
-        z-index="30"
-        bottom="0"
-        top="6rem"
-        left="max(0px, calc(50% - 45rem))"
-        right="auto"
-        width="19.5rem"
-        pb="10"
-        px="8"
-        overflow-y="auto"
-        overscroll-behavior="contain"
-      >
+      <chakra.div :display="{ base: 'none', lg: 'block' }" position="fixed" z-index="30" bottom="0" top="6rem"
+        left="max(0px, calc(50% - 45rem))" right="auto" width="19.5rem" pb="10" px="8" overflow-y="auto"
+        overscroll-behavior="contain">
         <sidebar />
       </chakra.div>
-      <chakra.main
-        :pl="{ base: 4, lg: '19.5rem' }"
-        pt="4"
-        pb="24"
-        :pr="{ base: 4, xl: 16 }"
-        class="chakra-prose"
-      >
+      <chakra.main :pl="{ base: 4, lg: '19.5rem' }" pt="4" pb="24" :pr="{ base: 4, xl: 16 }" class="chakra-prose">
         <chakra.div :mr="{ xl: '15.5rem' }">
           <slot />
 
           <c-h-stack align-items="center" mt="6">
             <c-icon name="info" />
-            <p>Edit this page on GitHub</p>
+            <c-link href="https://github.com/chakra-ui/vue-docs" is-external
+              :_hover="{ color: '#55c392', textDecoration: 'underline' }">
+              Edit this page on GitHub
+            </c-link>
           </c-h-stack>
         </chakra.div>
         <!-- <table-of-contents /> -->
