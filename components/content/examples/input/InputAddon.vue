308c42e43fa255f15076e214896128e9030b2952
Daniel Olabemiwo <44473671+D-Lite@users.noreply.github.com>
2023-06-10 11:53:10 +0100

    docs(input): add input documentation
    * docs(input): add input documentation
    
    * chore: code review fix

---
+++ components/content/examples/input/InputAddon.vue
@@ -0,0 +1,19 @@
+<template>
+    <c-stack spacing="4" w="xl" align-items="center">
+        <c-input-group>
+            <c-input-left-addon>+234</c-input-left-addon>
+            <c-input type="tel" rounded-left="0" placeholder="phone number" />
+        </c-input-group>
+        <!-- If you add the size prop to `InputGroup`, it'll pass it to all its children.  -->
+
+        <c-input-group>
+            <c-input-left-addon>https://</c-input-left-addon>
+            <c-input rounded="0" placeholder="mysite" />
+            <c-input-right-addon>.com</c-input-right-addon>
+        </c-input-group>
+    </c-stack>
+</template>
+
+<script setup>
+import { CInput, CStack, CInputGroup, CInputLeftAddon, CInputRightAddon } from '@chakra-ui/vue-next';
+</script>
---
+++ components/content/examples/input/InputFocus.vue
@@ -0,0 +1,12 @@
+<template>
+    <c-stack spacing="4" w="xl" align-items="center">
+        <c-input focus-border-color="lime" placeholder="Here is a sample placeholder" />
+        <c-input focus-border-color="pink.400" placeholder="Here is a sample placeholder" />
+        <c-input is-invalid error-border-color="red.300" placeholder="Here is a sample placeholder" />
+        <c-input is-invalid error-border-color="crimson" placeholder="Here is a sample placeholder" />
+    </c-stack>
+</template>
+
+<script setup>
+import { CInput, CStack } from '@chakra-ui/vue-next';
+</script>
