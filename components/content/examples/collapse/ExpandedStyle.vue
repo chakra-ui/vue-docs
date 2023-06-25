448deded7886f7488473569c396d5b0081e4479c
Daniel Olabemiwo <olabemiwodaniel@gmail.com>
2023-06-25 22:18:41 +0100

    feat: collapse component documentation and change markdown link in new tab

---
+++ components/content/examples/collapse/ExpandedStyle.vue
@@ -0,0 +1,22 @@
+<template>
+  <c-accordion>
+    <c-accordion-item>
+      <c-accordion-button :_expanded="{ bg: 'purple', color: 'white' }">
+        <c-v-stack align="flex-start" w="full">
+          Watercraft
+        </c-v-stack>
+        <c-accordion-icon element="c-accordion-item" />
+      </c-accordion-button>
+      <c-accordion-panel> Sample accordion content </c-accordion-panel>
+    </c-accordion-item>
+  </c-accordion>
+</template>
+
+<script setup>
+import {
+  CAccordion,
+  CAccordionItem,
+  CAccordionButton,
+  CAccordionPanel,
+} from "@chakra-ui/vue-next"
+</script>
