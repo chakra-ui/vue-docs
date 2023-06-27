dab049475fc3f2e0de2c7e07b5f720a84b03c574
Daniel Olabemiwo <olabemiwodaniel@gmail.com>
2023-06-27 11:26:30 +0100

    feat: simple grid component documentation

---
+++ components/content/examples/simplegrid/SimpleGridExample.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { CSimpleGrid, CBox } from '@chakra-ui/vue-next';
+</script>
+
+<template>
+  <c-simple-grid :columns="2" :spacing="10">
+    <c-box background="green" height="80px"></c-box>
+    <c-box background="red" height="80px"></c-box>
+    <c-box background="blue.900" height="80px"></c-box>
+    <c-box background="darkorange" height="80px"></c-box>
+    <c-box background="yellow" height="80px"></c-box>
+  </c-simple-grid>
+</template>
