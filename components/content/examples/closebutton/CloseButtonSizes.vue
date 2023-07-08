aa91d4e1960b8fe121e585d94544b9383f7621b2
carwack <sybren.willemot@gmail.com>
2023-07-08 15:34:59 +0200

    refactor: change name casing close button examples

--- components/content/examples/closebutton/CloseButtonSizes.vue
+++ components/content/examples/closebutton/CloseButtonSizes.vue
@@ -1,8 +1,9 @@
-<template>
-  <c-close-button size="sm" @click="handleClose" />
-  <c-close-button ml="6" size="md" @click="handleClose" />
-  <c-close-button ml="6" size="lg" @click="handleClose" />
-</template>
 <script setup>
 import { CCloseButton } from '@chakra-ui/vue-next';
 </script>
+
+<template>
+  <CCloseButton size="sm" @click="handleClose" />
+  <CCloseButton ml="6" size="md" @click="handleClose" />
+  <CCloseButton ml="6" size="lg" @click="handleClose" />
+</template>
