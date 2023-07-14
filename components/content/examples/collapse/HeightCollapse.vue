6b994d515b2ec6c976915dd64fc0cb83227a494c
Daniel Olabemiwo <olabemiwodaniel@gmail.com>
2023-07-14 13:33:37 +0100

    refactor: change name casing collapse

--- components/content/examples/collapse/HeightCollapse.vue
+++ components/content/examples/collapse/HeightCollapse.vue
@@ -1,23 +1,18 @@
 <template>
-  <c-box>
-    <c-button mb="4" variant-color="blue" @click="show = !show">
+  <CBox>
+    <CButton mb="4" variant-color="blue" @click="show = !show">
       Show {{ show ? 'Less' : 'More' }}
-    </c-button>
-    <c-collapse :is-open="show" :starting-height="20">
+    </CButton>
+    <CCollapse :is-open="show" :starting-height="20">
       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
       standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
       type specimen book.
-    </c-collapse>
-  </c-box>
+    </CCollapse>
+  </CBox>
 </template>
 
 
 <script setup>
-import {
-  CBox,
-  CButton,
-  CCollapse,
-} from "@chakra-ui/vue-next";
 import { ref } from 'vue';
 
 const show = ref(false);
--- components/content/examples/collapse/SimpleCollapse.vue
+++ components/content/examples/collapse/SimpleCollapse.vue
@@ -1,22 +1,17 @@
 <template>
-  <c-box>
-    <c-button mb="4" variant-color="blue" @click="show = !show">
+  <CBox>
+    <CButton mb="4" variant-color="blue" @click="show = !show">
       Toggle
-    </c-button>
-    <c-collapse :is-open="show">
+    </CButton>
+    <CCollapse :is-open="show">
       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
       standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
       type specimen book.
-    </c-collapse>
-  </c-box>
+    </CCollapse>
+  </CBox>
 </template> 
 
 <script setup>
-import {
-  CBox,
-  CButton,
-  CCollapse,
-} from "@chakra-ui/vue-next";
 import { ref } from 'vue';
 
 const show = ref(false);
