d3311eae6ba7d8f6d2e7c3d5f67250fa0376b58c
Daniel Olabemiwo <olabemiwodaniel@gmail.com>
2023-07-14 15:24:17 +0100

    refactor: change name casing wrap

--- components/content/examples/wrap/SpacingWrap.vue
+++ components/content/examples/wrap/SpacingWrap.vue
@@ -1,33 +1,29 @@
 <template>
-  <c-wrap spacing='30px'>
-    <c-wrap-item>
-      <c-center w='180px' h='80px' bg='red.200'>
+  <CWrap spacing='30px'>
+    <CWrapItem>
+      <CCenter w='180px' h='80px' bg='red.200'>
         Box 1
-      </c-center>
-    </c-wrap-item>
-    <c-wrap-item>
-      <c-center w='180px' h='80px' bg='green.200'>
+      </CCenter>
+    </CWrapItem>
+    <CWrapItem>
+      <CCenter w='180px' h='80px' bg='green.200'>
         Box 2
-      </c-center>
-    </c-wrap-item>
-    <c-wrap-item>
-      <c-center w='180px' h='80px' bg='tomato'>
+      </CCenter>
+    </CWrapItem>
+    <CWrapItem>
+      <CCenter w='180px' h='80px' bg='tomato'>
         Box 3
-      </c-center>
-    </c-wrap-item>
-    <c-wrap-item>
-      <c-center w='180px' h='80px' bg='blue.200'>
+      </CCenter>
+    </CWrapItem>
+    <CWrapItem>
+      <CCenter w='180px' h='80px' bg='blue.200'>
         Box 4
-      </c-center>
-    </c-wrap-item>
-    <c-wrap-item>
-      <c-center w='180px' h='80px' bg='purple.200'>
+      </CCenter>
+    </CWrapItem>
+    <CWrapItem>
+      <CCenter w='180px' h='80px' bg='purple.200'>
         Box 5
-      </c-center>
-    </c-wrap-item>
-  </c-wrap>
+      </CCenter>
+    </CWrapItem>
+  </CWrap>
 </template>
-
-<script setup>
-import { CWrap, CWrapItem, CCenter } from "@chakra-ui/vue-next"
-</script>
