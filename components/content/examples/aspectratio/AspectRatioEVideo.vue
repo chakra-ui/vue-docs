b1940d364f624f9c713e5751720dacae4a5e5dd5
Daniel Olabemiwo <olabemiwodaniel@gmail.com>
2023-07-16 22:11:20 +0100

    feat: aspect ratio documentation

---
+++ components/content/examples/aspectratio/AspectRatioEVideo.vue
@@ -0,0 +1,7 @@
+<template>
+  <CAspectRatio max-w="600px" max-h="300px" :ratio="4 / 3">
+    <CBox as="iframe" w="600px" h="500px"
+      title="Sybren Willemot - Can you build a design system with Chakra UI Vue? - Vuejs Amsterdam 2023"
+      src="https://www.youtube.com/embed/UL9Fcx6qDdk" allow-full-screen />
+  </CAspectRatio>
+</template>
