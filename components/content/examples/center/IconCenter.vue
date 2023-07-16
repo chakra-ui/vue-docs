75ccf7b56d373f866dbce098cc46e7db2749f201
Daniel Olabemiwo <44473671+D-Lite@users.noreply.github.com>
2023-07-16 11:07:11 +0100

    Center Component (#44)
    * feat: center component documentation
    
    * refactor: change name casing center
    
    * refactor: change name casing center

---
+++ components/content/examples/center/IconCenter.vue
@@ -0,0 +1,13 @@
+<template>
+  <CHStack>
+    <CCenter w='40px' h='40px' bg='tomato' color='white'>
+      <CIcon name="phone" />
+    </CCenter>
+    <CCenter w='40px' h='40px' bg='tomato' color='white'>
+      <CBox as='span' fontWeight='bold' fontSize='lg'>
+        1
+      </CBox>
+    </CCenter>
+  </CHStack>
+</template>
+
---
+++ components/content/examples/center/SimpleCenter.vue
@@ -0,0 +1,5 @@
+<template>
+  <CCenter bg='tomato' h='100px' color='white'>
+    This is the Center
+  </CCenter>
+</template>
