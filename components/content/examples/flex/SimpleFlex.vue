e2d019275f9f9210863c8f9031497359ea426ba9
Daniel Olabemiwo <44473671+D-Lite@users.noreply.github.com>
2023-08-03 16:49:26 +0100

    Flex Component (#45)
    * feat: flex component documentation
    
    * refactor: change name casing FLEX
    
    * refactor: change name casing flex
    
    * chore: flex documentation review

---
+++ components/content/examples/flex/SimpleFlex.vue
@@ -0,0 +1,13 @@
+<template>
+  <CFlex color='white'>
+    <CCenter w='100px' bg='green.500'>
+      <CText>Box 1</CText>
+    </CCenter>
+    <CSquare bg='blue.500' size='150px'>
+      <CText>Box 2</CText>
+    </CSquare>
+    <CBox flex='1' bg='tomato'>
+      <CText>Box 3</CText>
+    </CBox>
+  </CFlex>
+</template>
---
+++ components/content/examples/flex/SpacerFlex.vue
@@ -0,0 +1,11 @@
+<template>
+  <CFlex>
+    <CBox p='4' bg='red.400'>
+      Box 1
+    </CBox>
+    <CSpacer />
+    <CBox p='4' bg='green.400'>
+      Box 2
+    </CBox>
+  </CFlex>
+</template>
