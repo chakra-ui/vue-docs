856c285b1a9a00f6905292d6b3c7ee324e8ab3a8
Daniel Olabemiwo <44473671+D-Lite@users.noreply.github.com>
2023-08-03 16:50:33 +0100

    Stack component (#48)
    * feat: stack component documentation
    
    * refactor: change name casing stack
    
    * refactor: change name casing stack
    
    * chore: stack documentation review

---
+++ components/content/examples/stack/HorizontalStack.vue
@@ -0,0 +1,12 @@
+<template>
+  <CStack :spacing="5" is-inline>
+    <CBox :p="5" shadow="md" border-width="1px">
+      <CHeading>See the Vue</CHeading>
+      <CText :mt="4">Vue makes front-end development a breeze.</CText>
+    </CBox>
+    <CBox :p="5" shadow="md" border-width="1px">
+      <CHeading>Go Nuxt!</CHeading>
+      <CText :mt="4">Nuxt makes writing Vue even easier.</CText>
+    </CBox>
+  </CStack>
+</template>
