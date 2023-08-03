3005a79125afcbb6f80a77b89941b94a3ff705dd
Daniel Olabemiwo <44473671+D-Lite@users.noreply.github.com>
2023-08-03 16:53:27 +0100

    feat: aspect ratio documentation (#51)

---
+++ components/content/examples/aspectratio/AspectRatioEMap.vue
@@ -0,0 +1,7 @@
+<template>
+  <CAspectRatio :ratio="16 / 9">
+    <CBox as="iframe"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126614.51346341768!2d3.8226594639678577!3d7.387080139026444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10398d77eeff086f%3A0x3b33e0f76e8e04a9!2sIbadan%2C%20Oyo!5e0!3m2!1sen!2sng!4v1689540977837!5m2!1sen!2sng"
+      frameborder="0" aria-hidden="false" tabindex="0" alt="Ibadan, Nigeria" allow-full-screen />
+  </CAspectRatio>
+</template>
