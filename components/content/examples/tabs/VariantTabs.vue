ec315296a376c37ae4a61b91318cd0d10f4d1a3d
Daniel Olabemiwo <44473671+D-Lite@users.noreply.github.com>
2023-08-10 17:13:21 +0100

    Input component (#50)
    * docs(input): add input documentation
    
    * chore: code review fix
    
    * refactor: change name casing input
    
    * chore: input documentation review

---
+++ components/content/examples/tabs/VariantTabs.vue
@@ -0,0 +1,36 @@
+<template>
+  <CBox w="600px">
+    <CTabs variant="enclosed-colored">
+      <CTabList v-model="selected">
+        <CTab value='1'>One</CTab>
+        <CTab value='2'>Two</CTab>
+        <CTab value='3'>Three</CTab>
+      </CTabList>
+
+      <CTabPanels v-model="selected">
+        <CTabPanel value='1'>
+          <p>Panel One</p>
+        </CTabPanel>
+        <CTabPanel value='2'>
+          <p>Panel Two</p>
+        </CTabPanel>
+        <CTabPanel value='3'>
+          <p>Panel Three</p>
+        </CTabPanel>
+      </CTabPanels>
+    </CTabs>
+  </CBox>
+</template> 
+
+<script setup>
+import { ref } from 'vue';
+import {
+  CTabs,
+  CTabList,
+  CTabPanels,
+  CTab,
+  CTabPanel
+} from "@chakra-ui/vue-next";
+
+const selected = ref(1);
+</script>
