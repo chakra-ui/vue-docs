f40cacba92374c2e99843b96ff4de85b6fe18699
carwack <sybren.willemot@gmail.com>
2023-10-07 18:34:12 +0200

    chore: update deps

---
+++ components/content/examples/FormControl/SimpleFormControl.vue
@@ -0,0 +1,7 @@
+<template>
+  <CFormControl>
+    <CFormLabel>Email address</CFormLabel>
+    <CInput type='email' />
+    <CFormHelperText>We'll never share your email.</CFormHelperText>
+  </CFormControl>
+</template>
