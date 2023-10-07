f37c82833720e04384b27660784cb22acca233b2
carwack <sybren.willemot@gmail.com>
2023-10-07 20:44:03 +0200

    Revert "chore: update deps"
    This reverts commit f40cacba92374c2e99843b96ff4de85b6fe18699.

--- components/content/examples/FormControl/SimpleFormControl.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <CFormControl>
-    <CFormLabel>Email address</CFormLabel>
-    <CInput type='email' />
-    <CFormHelperText>We'll never share your email.</CFormHelperText>
-  </CFormControl>
-</template>
